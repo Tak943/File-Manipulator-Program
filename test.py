ee335ada0cd9b8cca51886d298825eed12140426
Tak943 <fukumototakayashusyoku@gmail.com>
2026-07-05 19:47:37 +0900

    add: test open()

---
+++ test.py
@@ -0,0 +1,7 @@
+pathname = "./test.txt"
+contents = ""
+with open(pathname) as f:
+    contents = f.read()
+
+with open(pathname, "w") as f:
+    f.write(contents + "\Appending more text to this file!")
